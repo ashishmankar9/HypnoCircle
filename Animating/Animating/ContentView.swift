643f28007cc9237a1bbc38188f8bbd6dc4d441ba
Ashish Mankar <ashish.m@peopleinteractive.in>
2025-01-02 14:03:36 +0530

    Renaming

--- Animating/Animating/ContentView.swift
+++ Animating/Animating/ContentView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var outerAnimation = false
-    @State var middleAnimation = false
-    @State var innerAnimation = false
+    @State var startAnimation = false
     
     var body: some View {
         ZStack {
@@ -19,12 +17,12 @@ struct ContentView: View {
                     .fill((Int(index) % 2 == 0) ? Color.black : Color.white)
                     .shadow(radius: 5)
                     .padding(12 * CGFloat(index))
-                    .scaleEffect(outerAnimation ? 0.96 : 1)
-                    .animation(.easeInOut(duration: 1).repeatForever().delay(Double(index) * 0.2), value: outerAnimation)
+                    .scaleEffect(startAnimation ? 0.96 : 1)
+                    .animation(.easeInOut(duration: 1).repeatForever().delay(Double(index) * 0.2), value: startAnimation)
             }
         }
         .onAppear(perform: {
-            outerAnimation.toggle()
+            startAnimation.toggle()
         })
     }
 }
